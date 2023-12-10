1bd654714575cd86508ccf55e0e350e47bc21d3c
profiler00 <kftjungle@gmail.com>
2023-12-10 16:12:00 +0900

    feat(App): 상수 및 메세지 모듈화

---
+++ src/constants/constants.js
@@ -0,0 +1,24 @@
+const lotto = {
+  LENGTH: 6,
+  PRICE: 1000,
+  MIN_RANGE: 1,
+  MAX_RANGE: 45,
+};
+
+const lottoCount = {
+  FIFTH: 3,
+  FOURTH: 4,
+  THIRD: 5,
+  SECOND: 6,
+  FIRST: 7,
+};
+
+const lottoPrize = {
+  FIFTH: 5000,
+  FOURTH: 50000,
+  THIRD: 1500000,
+  SECOND: 30000000,
+  FIRST: 2000000000,
+};
+
+export { lotto, lottoCount, lottoPrize };
